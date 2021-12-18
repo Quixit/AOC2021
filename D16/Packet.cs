764ce4f14b8a535821357e91a6eabf3c1ba8940c
Jonathan Bruce <jonathanb@spswave.com>
2021-12-18 14:54:13 -0500

    Sixteen

---
+++ D16/Packet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace D16
+{
+    public class Packet
+    {
+        public int Version { get; set; }
+        public int Type { get; set; }
+        public long Value { get; set; }
+
+        public int LengthType { get; set; }
+
+        public Packet(string header)
+        {
+            Version = Convert.ToInt32(header.Substring(0,3), 2);
+            Type = Convert.ToInt32(header.Substring(3,3), 2);
+        }
+    }
+}
